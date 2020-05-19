92d0c4924119daddc6683e942bd499b51b1ba923
skuley <skuleyandpotato@gmail.com>
2020-05-19 14:15:04 +0900

    2020-05-19 14:15

--- JSP07_ServletContext/src/com/lec/servlet/ServletContextEx.java
+++ JSP07_ServletContext/src/com/lec/servlet/ServletContextEx.java
@@ -3,43 +3,36 @@ package com.lec.servlet;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns = {"/InitS"},
-		initParams = {
-				@WebInitParam(name = "id", value = "test11"),
-				@WebInitParam(name = "pw", value = "1234"),
-				@WebInitParam(name = "local", value = "Pusan"),
-		})
-public class InitServlet extends HttpServlet {
+@WebServlet("/SCEx")
+public class ServletContextEx extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    public InitServlet() {
+    public ServletContextEx() {
         super();
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// ServletConfig 의 메소드를 사용. 
-		String id = getInitParameter("id");
-		String pw = getInitParameter("pw");
-		String local = getInitParameter("local");
+		// 현재 이 서블릿이 ..??
+		String id = getServletContext().getInitParameter("id");
+		String password = getServletContext().getInitParameter("password");
+		String local = getServletContext().getInitParameter("local");
 		
 		response.setContentType("text/html; charset=utf-8");
+		
 		PrintWriter out = response.getWriter();
 		
 		out.println("id : " + id + "<br>");
-		out.println("pw : " + pw + "<br>");
+		out.println("password : " + password + "<br>");
 		out.println("local : " + local + "<br>");
 		
-		
 		out.close();
-	} // doGet()
+	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doGet(request, response);
